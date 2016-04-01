6e146f6bf580b1790b29113b74f4aa42fb2a67f4
xiaokai <kexiaokai@gmail.com>
2016-04-01 18:15:19 +0800

    [UPD] add example

--- js/mathm.js
+++ js/mathm.js
@@ -1,24 +1,33 @@
 new Vue({
   el: '#editor',
   data: {
-    input: '# hello'
+    input: ''
   },
   filters: {
     marked: function(s){
     	var markdown = marked(s);
-    	
     	return markdown;
     }
   },
   watch: {
   	input: function(){
+  		this.udpateMath();
+  	}
+  },
+  ready: function(){
+  	var that = this;
+  	
+  	setTimeout(function(){
+  		that.input = '![神算子](http://img.jyeoo.net/quiz/images/201204/4/53aae10e.png#right)如图，长方形ABCD，设其长AD=a，宽AB=b（a＞b），在BC边上选取一点E，将△ABE沿AE翻折后B至直线BD上的O点，若O为长方形ABCD的对称中心，则$\\frac{a}{b}$的值是_____．'
+  	}, 1000);
+  	
+  },
+  methods:{
+  	udpateMath: function(){
   		MathJax.Hub.Queue(
   			['resetEquationNumbers', MathJax.InputJax.TeX], 
   			['Typeset', MathJax.Hub, this.$els.output]
-  		)
+  		);
   	}
-  },
-  methods:{
-  	
   }
 });
