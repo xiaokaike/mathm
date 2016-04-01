269eff851bace246db475e7e091296fbb9c862b7
xiaokai <kexiaokai@gmail.com>
2016-04-01 18:15:47 +0800

    [UPD] udpate

--- js/mathm.js
+++ js/mathm.js
@@ -18,7 +18,7 @@ new Vue({
   	var that = this;
   	
   	setTimeout(function(){
-  		that.input = '![神算子](http://img.jyeoo.net/quiz/images/201204/4/53aae10e.png#right)如图，长方形ABCD，设其长AD=a，宽AB=b（a＞b），在BC边上选取一点E，将△ABE沿AE翻折后B至直线BD上的O点，若O为长方形ABCD的对称中心，则$\\frac{a}{b}$的值是_____．'
+  		that.input = '![XXX](http://img.jyeoo.net/quiz/images/201204/4/53aae10e.png#right)如图，长方形ABCD，设其长AD=a，宽AB=b（a＞b），在BC边上选取一点E，将△ABE沿AE翻折后B至直线BD上的O点，若O为长方形ABCD的对称中心，则$\\frac{a}{b}$的值是_____．'
   	}, 1000);
   	
   },
