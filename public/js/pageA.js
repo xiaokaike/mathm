a4cd6a869baa3b75087b629fd8259769a5cf317e
xiaokai <kexiaokai@gmail.com>
2016-04-12 22:10:21 +0800

    [UPD] float right

--- public/js/pageA.js
+++ public/js/pageA.js
@@ -10206,6 +10206,34 @@
 	});
 
 
+
+
+	md.renderer.rules.image = function(tokens, idx, options, env, self){
+	  
+	  var token = tokens[idx]
+
+	  var src = token.attrs[0][1]
+	  var exp = src.match(/#(.*)/)
+	  var styl = 'float: $;'
+	  exp = exp[1] ? exp[1] : null
+
+	  if(exp === 'right' || exp === 'left'){
+	    styl = styl.replace('$', exp)
+	    token.attrs.push(['style', styl])
+	  }
+
+	  // "alt" attr MUST be set, even if empty. Because it's mandatory and
+	  // should be placed on proper position for tests.
+	  //
+	  // Replace content with actual value
+
+	  token.attrs[token.attrIndex('alt')][1] =
+	    self.renderInlineAsText(token.children, options, env);
+
+	  return self.renderToken(tokens, idx, options);
+	}
+
+
 	module.exports = md 
 
 /***/ },
